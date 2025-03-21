4418cf602ba5778886af64664605bc7473b66e9a
Parker Blackwell <parkerblackwell7@yahoo.com>
2025-03-21 12:21:17 -0500

    Fixing logo covering content on mobile

--- app.js
+++ app.js
@@ -1,7 +1,11 @@
 const menu = document.querySelector('#mobile-menu');
 const menuLink = document.querySelector('.navbar__menu');
+const logo = document.querySelector('.logo');
+const leftLogoFill = document.querySelector('.left_logo_fill');
 
 menu.addEventListener('click', function() {
     menu.classList.toggle('is-active');
     menuLink.classList.toggle('active');
+    logo.classList.toggle('active');
+    leftLogoFill.classList.toggle('active');
 });
